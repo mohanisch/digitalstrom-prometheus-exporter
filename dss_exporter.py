435691b4727236ead0be6a4191c96c096d56e47e
Marco Hanisch <marco.hanisch@webkompleks.de>
2021-06-07 06:16:19 +0200

    Add metername and vdc to device metric

--- dss_exporter.py
+++ dss_exporter.py
@@ -76,7 +76,7 @@ class DssCollector(object):
 
         for circuit_data in circuits_list:
             if circuit_data['dsid']:
-                circuit_details = {'name': circuit_data['name'], 'dsid': circuit_data['dsid']}
+                circuit_details = {'name': circuit_data['name'], 'dsid': circuit_data['dsid'], 'hwName': circuit_data['hwName']}
                 circuits.append(circuit_details)
 
         return circuits
@@ -113,34 +113,36 @@ class DssCollector(object):
         dcc = GaugeMetricFamily(
             'dss_circuit_consumption',
             'Current power consumption [W]',
-            labels=["circuit", "fstype"])
+            labels=["circuit", "hwName"])
         dcm = GaugeMetricFamily(
             'dss_circuit_metervalue',
             'Current measurent of the power consumption [Ws]',
-            labels=["circuit", "fstype"])
+            labels=["circuit", "hwName"])
 
         for circuit in self._circuits:
             circuit_name = self._mount_point(circuit['name'])
+            circuit_type = self._mount_point(circuit['hwName'])
             circuit_consumption = (self.collect_circuit_details(circuit['dsid'], 'getConsumption'))
             circuit_energymetervalue = (self.collect_circuit_details(circuit['dsid'], 'getEnergyMeterValue'))
 
             dcc.add_metric(
-                [circuit_name, "fsname", circuit['name']], circuit_consumption['consumption'])
+                [circuit_name, circuit_type, circuit['name']], circuit_consumption['consumption'])
 
             dcm.add_metric(
-                [circuit_name, "fsname", circuit['name']], circuit_energymetervalue['meterValue'])
+                [circuit_name, circuit_type, circuit['name']], circuit_energymetervalue['meterValue'])
         yield dcc
         yield dcm
 
         ddp = GaugeMetricFamily(
             'dss_device_is_present',
             'Current state of device',
-            labels=["device", "fstype"])
+            labels=["device", "meterName", "vdc"])
         for device in self.collect_devices():
             device_name = self._mount_point(device['name'])
+            device_vdc = str(device['isVdcDevice'])
 
             ddp.add_metric(
-                [device_name, "fsname", device_name], device['isPresent'])
+                [device_name, device['meterName'], device_vdc, device_name], device['isPresent'])
         yield ddp
 
         ### disabled as the response takes very long
