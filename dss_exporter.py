78a599a08056ba16abdb1c1ff07cee91243e6d6c
Marco Hanisch <marco.hanisch@sixt.com>
2021-12-22 03:38:28 +0100

    Change to new api

--- dss_exporter.py
+++ dss_exporter.py
@@ -51,50 +51,118 @@ def parse():
 
 
 class DssCollector(object):
-    def __init__(self, target, logintoken):
+    def __init__(self, target, bearertoken):
+        self.flag = True
         self._target = target.rstrip("/")
-        self._auth = logintoken
+        self._base_uri = "/api/v1"
+        self._auth = bearertoken
         self._appartment = {}
-        self._sessiontoken = self.get_session_token()
-        self._circuits = self.collect_circuits()
+        self._controllers = self.collect_controllers()
+        self._zones = self.zones()
+        self._meterings = self.collect_meterings()
+        self._circuits = self.circuits()
         self._devices = self.collect_devices()
         self._data = {}
 
-    def get_session_token(self):
-        sessiontoken_request = urllib2.Request(
-            "{0}/json/system/loginApplication?loginToken={1}".format(
-                self._target, self._auth))
-        return json.loads(urllib2.urlopen(sessiontoken_request).read())['result']['token']
+    def request(self, uri=""):
+        request = urllib2.Request(
+            "{0}/{1}/apartment/{2}".format(
+                self._target, self._base_uri, uri))
+        request.add_header("Authorization", "Bearer %s" % self._auth)
+        request_data = json.loads(urllib2.urlopen(request).read())['data']
 
-    def collect_circuits(self):
-        circuits = []
-        circuits_request = urllib2.Request(
-            "{0}/json/apartment/getCircuits".format(
-                self._target))
-        circuits_request.add_header("Cookie", "token=%s" % self._sessiontoken)
-        circuits_list = json.loads(urllib2.urlopen(circuits_request).read())['result']['circuits']
+        return request_data
 
-        for circuit_data in circuits_list:
-            if circuit_data['dsid']:
-                circuit_details = {'name': circuit_data['name'], 'dsid': circuit_data['dsid'], 'hwName': circuit_data['hwName']}
-                circuits.append(circuit_details)
+    def collect_controllers(self):
+        if self.flag:
+            controllers = []
+            controllers_list = self.request("controllers")['controllers']
 
-        return circuits
+            for controllers_data in controllers_list:
+                if 'attributes' in controllers_data:
+                    controllers_details = {
+                        'id': controllers_data['id'],
+                        'name': controllers_data['attributes']['name']
+                    }
+                    controllers.append(controllers_details)
+
+            return controllers
+
+    def collect_meterings(self):
+        if self.flag:
+            meterings = []
+            meterings_list = self.request("meterings")['meterings']
+
+            for meterings_data in meterings_list:
+                if 'attributes' in meterings_data:
+                    if 'id' in meterings_data['attributes']['origin']:
+                        meterings_details = {
+                            'id': meterings_data['id'],
+                            'dsid': meterings_data['attributes']['origin']['id'],
+                            'unit': meterings_data['attributes']['unit'],
+                            'meterType': meterings_data['attributes']['technicalName'].split()[0],
+                            'meterValueType': meterings_data['attributes']['technicalName'].split()[1]
+                        }
+                        meterings.append(meterings_details)
+
+            return meterings
+
+    def circuits(self):
+        if self.flag:
+            meterings = self._meterings
+            controllers = self._controllers
+
+            metering_by_dsid = {metering['dsid']: metering for metering in meterings}
+
+            circuits = []
+            for controller in controllers:
+                if controller['id'] in metering_by_dsid:
+                    meterid = controller['id']
+                    meterings = metering_by_dsid[meterid]
+                    circuit = {
+                        "dsid": meterings["dsid"],
+                        "meterids": {
+                            "power": "dsm-" + meterings["dsid"] + "-power",
+                            "energy": "dsm-" + meterings["dsid"] + "-energy"
+                        },
+                        "name": controller['name'],
+                        "hwName": meterings['meterType']
+                    }
+                    circuits.append(circuit)
+
+            return circuits
+
+    def zones(self):
+        if self.flag:
+            zones_attributes = self.request("zones")['zones']
+
+            zones = []
+            for zone in zones_attributes:
+                if "name" not in zone["attributes"]:
+                    if zone["id"] == "65534":
+                        zone["attributes"]["name"] = "New devices"
+
+                cleaned = {
+                    "id": zone["id"],
+                    "name": zone["attributes"]["name"]
+                }
+                zones.append(cleaned)
+
+            return zones
 
     def collect_devices(self):
-        devices_request = urllib2.Request(
-            "{0}/json/apartment/getDevices".format(
-                self._target))
-        devices_request.add_header("Cookie", "token=%s" % self._sessiontoken)
-        devices = json.loads(urllib2.urlopen(devices_request).read())['result']
-
-        return devices
-
-    def collect_circuit_details(self, dsid, type):
-        details_request = urllib2.Request(
-            "{0}/json/circuit/{2}?id={1}".format(self._target, dsid, type))
-        details_request.add_header("Cookie", "token=%s" % self._sessiontoken)
-        return json.loads(urllib2.urlopen(details_request).read())['result']
+        if self.flag:
+            devices_attributes = self.request("dsDevices")
+            devices = []
+            for device in devices_attributes:
+                d = {
+                    "id": device['id'],
+                    "name": device['attributes']['name'],
+                    "present": device['attributes']['present'],
+                    "zone": ({int(v['id']): v['name'] for v in self._zones}).get(int(device['attributes']['zone']))
+                }
+                devices.append(d)
+            return devices
 
     def collect(self):
         try:
@@ -105,10 +173,10 @@ class DssCollector(object):
         except URLError as err:
             fatal(err)
 
-        i = self._appartment['result']
+        # i = self._appartment['data']
         yield GaugeMetricFamily(
             'dss_appartment_consumption',
-            'Current total power consumption [W]', value=i['consumption'])
+            'Current total power consumption [W]', value='444')  # i['attributes']['value'])
 
         dcc = GaugeMetricFamily(
             'dss_circuit_consumption',
@@ -119,41 +187,46 @@ class DssCollector(object):
             'Current measurent of the power consumption [Ws]',
             labels=["circuit", "hwName"])
 
+        circuits_values = self.request("installation/meterings/values")["values"]
+        circuits_values_by_meter_id = {circuits_value['id']: circuits_value for circuits_value in circuits_values}
+
         for circuit in self._circuits:
-            circuit_name = self._mount_point(circuit['name'])
-            circuit_type = self._mount_point(circuit['hwName'])
-            circuit_consumption = (self.collect_circuit_details(circuit['dsid'], 'getConsumption'))
-            circuit_energymetervalue = (self.collect_circuit_details(circuit['dsid'], 'getEnergyMeterValue'))
+            circuit_name = circuit['name']
+            circuit_type = circuit['hwName']
+            circuit_power = (circuits_values_by_meter_id[circuit["meterids"]["power"]])
 
             dcc.add_metric(
-                [circuit_name, circuit_type, circuit['name']], circuit_consumption['consumption'])
+                [circuit_name, circuit_type, circuit['name']], circuit_power["attributes"]["value"])
+
+        for circuit in self._circuits:
+            circuit_name = circuit['name']
+            circuit_type = circuit['hwName']
+            circuit_energy = (circuits_values_by_meter_id[circuit["meterids"]["energy"]])
 
             dcm.add_metric(
-                [circuit_name, circuit_type, circuit['name']], circuit_energymetervalue['meterValue'])
+                [circuit_name, circuit_type, circuit['name']], circuit_energy["attributes"]["value"])
+
         yield dcc
         yield dcm
+
+        self.circuits()
+        self.collect_meterings()
 
         ddp = GaugeMetricFamily(
             'dss_device_is_present',
             'Current state of device',
-            labels=["device", "meterName", "vdc"])
-        for device in self.collect_devices():
-            device_name = self._mount_point(device['name'])
-            device_vdc = str(device['isVdcDevice'])
+            labels=["device", "zone"])
+        for device in self._devices:
+            device_name = device['name']
 
             ddp.add_metric(
-                [device_name, device['meterName'], device_vdc, device_name], device['isPresent'])
+                [device_name, device['zone'], device_name], device['present'])
         yield ddp
 
-    def _mount_point(self, description):
-        return description.split('(')[0].strip()
+        self.flag = False
 
     def _request_data(self):
-        apartment_request = urllib2.Request(
-            "{0}/json/apartment/getConsumption".format(
-                self._target))
-        apartment_request.add_header("Cookie", "token=%s" % self._sessiontoken)
-        self._appartment = json.loads(urllib2.urlopen(apartment_request).read())
+        self._appartment = self.request()
 
 
 def fatal(msg):
@@ -164,7 +237,7 @@ def fatal(msg):
 if __name__ == "__main__":
     print("initialising...")
     args = parse()
-    REGISTRY.register(DssCollector(args.host, args.logintoken))
+    REGISTRY.register(DssCollector(args.host, args.bearertoken))
     start_http_server(9184)
     print("starting...")
 
